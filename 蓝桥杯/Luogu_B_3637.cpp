ec6297a028ea7d3863cd79980f06224b4181bcd9
Eviden0 <1538279017@qq.com>
2024-04-09 23:33:40 +0800

    memset有坑!

--- 蓝桥杯/Luogu_B_3637.cpp
+++ 蓝桥杯/Luogu_B_3637.cpp
@@ -7,6 +7,7 @@ int main(){
     int dp[5050];
     // memset(dp,1,sizeof(dp));
     for(int i=0;i<5050;i++)dp[i]=1;
+    //所以只能将int类型的数组初始化为0和-1这两个值。
     // for(int i=1;i<=n;i++)cout<<dp[i]<<' ';
     for(int i=1;i<=n;i++)cin>>m[i];
     for(int i=1;i<=n;i++){
