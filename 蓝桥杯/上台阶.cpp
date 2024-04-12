f0c2e16ef3c5847feb9f3aa29516841540da4db5
Eviden0 <1538279017@qq.com>
2024-04-12 19:38:37 +0800

    dfs

--- 蓝桥杯/上台阶.cpp
+++ 蓝桥杯/上台阶.cpp
@@ -13,22 +13,47 @@
 // }
 //记忆化搜索
 
+// #include<bits/stdc++.h>
+// // #define int long long ;
+// int mem[100];
+// int dfs(int x){
+//     if(mem[x])return mem[x];
+//     int sum=0;
+//     if(x==1)sum=1;
+//     else if(x==2)sum=2;
+//     else sum=dfs(x-1)+dfs(x-2);
+//     mem[x]=sum;
+//     return sum;
+// }
+// using namespace std;
+// signed main(){
+// int n;cin>>n;
+// int res = dfs(n);
+// cout<<res;
+// return 0;
+// }
+
 #include<bits/stdc++.h>
-// #define int long long ;
-int mem[100];
-int dfs(int x){
-    if(mem[x])return mem[x];
-    int sum=0;
-    if(x==1)sum=1;
-    else if(x==2)sum=2;
-    else sum=dfs(x-1)+dfs(x-2);
-    mem[x]=sum;
-    return sum;
-}
 using namespace std;
-signed main(){
-int n;cin>>n;
-int res = dfs(n);
-cout<<res;
-return 0;
+int n,cnt;
+int mem[100];
+void dfs1(int x){
+    //纯暴力搜
+    if(x>n)return ;
+    if(x==n)cnt++;
+    dfs1(x+1);
+    dfs1(x+2);
+}
+//最少(n-1)/2+1次
+//最多n次
+int dfs2(int x){
+    if(x==1||x==2)return x;
+    
+    return dfs2(x-1)+dfs2(x-2);
+}
+int main(){
+    cin>>n;
+    dfs1(0);
+    cout<<cnt;
+    cout<<endl<<dfs2(n);
 }
