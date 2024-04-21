f1e3f94c7d2d3c3e98f42511875db609826e26dc
Eviden0 <1538279017@qq.com>
2024-04-21 17:15:50 +0800

    oj

--- Xtuoj/1576.cpp
+++ Xtuoj/1576.cpp
@@ -1,8 +1,17 @@
 #include<iostream>
-
+#include<vector>
+#include<set>
+#include<cmath>
+#include<algorithm>
 using namespace std;
 // int a[100010];
 int sum[100010];
+vector<int> yinzi;
+void yinz(int s){
+    for(int i=1;i<=sqrt(s);i++){
+        if(s%i==0){yinzi.push_back(i);yinzi.push_back(s/i);}
+    }
+}
 int main(){
     int n;
     cin>>n;
@@ -10,43 +19,41 @@ int main(){
     //前缀和 哟哟哟
     //貌似不需要存原数组???
     //1 2 3 1 2 3
-    //1 3 6 7 9 12
-
-
+    //1 3 6 7 9 13
 
     //4 1 3 2 4 1
     //4 5 8 10 14 15
     //5
-    int mm=-1;
+    // int mm=-1;
     int sumx=0,a;
 
     for(int i=1;i<=n;i++){
         scanf("%d",&a);
         sumx+=a;
-        mm=max(a,mm);
+        // mm=max(a,mm);
         sum[i]=sumx;
         // cout<<sum[i]<<' ';
     }
     // cout<<endl;
     int tm=0;
-    // cout<<sumx;
-    bool flag=false;
-    for(int r=mm;r<=sumx;r++){
+    yinz(sumx);
+    // set<int> yinzi1(yinzi.begin(),yinzi.end());
+    sort(yinzi.begin(),yinzi.end());
+    if(!sumx){cout<<0<<endl;}//狗东西!非负是吧,真恶心!
+    for(auto r=yinzi.begin();r!=yinzi.end();r++){
         int i=1;
+        int cnt=0;
         for(;i<=n;i++){
-            if(sum[i]-tm>r)break;
-            if(sum[i]-tm==r){
+            if(sum[i]-tm>*r)break;
+            if(sum[i]-tm==*r){
                 tm=sum[i];
-                flag=true;
+                cnt++;
             }
         }
-        if(i>=n&&flag){
-            cout<<r<<endl;
+        if(i>=n&&sumx/cnt==*r){
+            cout<<*r<<endl;
             break;
         }
         tm=0;
     }
-    //做法有问题na!
-    //等下再好好想想
-
 }
--- Xtuoj/test.cpp
+++ Xtuoj/test.cpp
@@ -1,60 +1,18 @@
-#include<stdio.h>
-char ch[2][8];
-char ch1[2][8];
+#include<vector>
+#include<iostream>
+#include<cmath>
+using namespace std;
+vector<int> yinzi;
+void yinz(int s){
+    for(int i=1;i<=sqrt(s);i++){
+        if(s%i==0){yinzi.push_back(i);yinzi.push_back(s/i);}
+    }
+}
 int main(){
-    int a,n=1,flag1=0,flag2=0,flag3=0;
-    
-    n=1;
-    int count=1;
-    while(scanf("%d %s %s %s %s",&a,&ch[0],&ch[1],&ch1[0],&ch1[1])!=EOF){
-        getchar();
-        if(flag3==0){
-        if(n==1){
-            printf("Alice start game %d\n",count);
-        }
-        else{
-            printf("Bob start game %d\n",count);
-        }
+    int s=100010;
+    yinz(s);
+    // for(int i=1;i<=10;i++)in.push_back(i);
+    for(auto i =yinzi.begin();i!=yinzi.end();i++){
+        cout<<*i<<' ';
     }
-        flag3=1;
-        int sum=0;
-        for(int i=0;i<2;i++){
-            if(ch[i][0]=='P')
-            sum+=5;
-            if(ch1[i][0]=='P'){
-                sum+=5;
-            }
-           
-        }
-        if(sum==a){
-            flag3=0;
-            if(n==1){
-                    printf("Alice win game %d\n",count);
-                    flag1++;
-                    
-            }
-            else{
-                printf("Bob win game %d\n",count);
-                flag2++;
-            }
-                count++;
-        }
-        else{
-            if(n==1){
-                printf("Change to Bob\n");
-                n=2;
-            }
-            else{
-                printf("Change to Alice\n");
-                n=1;
-            }
-        }
-        
-    }
-    printf("Game over %d:%d\n",flag1,flag2);
-    
-    return 0;
-} 
-
-
-//202205566502
+}
