6a9967e936b287fa6948ee400599eaca282d0d0d
Eviden0 <1538279017@qq.com>
2024-04-22 23:27:33 +0800

    递归

--- Luogu_P_1928.cpp
+++ Luogu_P_1928.cpp
@@ -20,7 +20,29 @@ string read(){
     }
     return s;
 }
+string digui(){
+    string r="",tr="";
+    int n;
+    char c;
+    while (cin>>c)
+    {
+        /* code */
+        if(c=='['){
+            cin>>n;
+            tr=digui();//递归搜索应该扩展的字符串
+            while(n--)r+=tr;//将递归搜索的结果保存结果字符串,有几个n就保存几次
+        }
+        else {
+            if(c==']')return r;
+            else r+=c;
+        }
+
+    }
+    cout<<r;
+    return r;
+}
 int main(){
-    cout<<read();
+    // cout<<read();
+    digui();
     return 0;
 }
