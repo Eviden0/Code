114ef0e7d181026f29e018612171afe6b5e7a258
Eviden0 <1538279017@qq.com>
2024-05-05 10:42:37 +0800

    oj

--- Xtuoj/1561.cpp
+++ Xtuoj/1561.cpp
@@ -2,34 +2,40 @@
 using namespace std;
 int n;
 int m;
-bool check(int *a,int *b,int x){
-    int sum=0;
+int res;
+bool check(int *a,int *b,long long x){
+    long long sum=0;
     for(int i=0;i<n;i++){
         // 知道做 5 个药
         sum+=((a[i]*x+b[i]-1)/b[i]);
     }
-    if(sum<=x)
-    return true;
+    if(sum<=m)return true;
     else return false;
 }
 int main(){
 
     int t;
+    // cin>>t;
+    scanf("%d",&t);
     while(t--){
         int need[1005]={0},cous[1005];
-        cin>>n>>m;
+        // cin>>n>>m;
+        scanf("%d %d",&n,&m);
         for(int i=0;i<n;i++)scanf("%d",&need[i]);
-        // a1 a2 a3 =1:1:1
-        // r*m1+s*m2+t*m3 =10;
-        long long l=1,r=10000000000;
-        long long mid=(r+l)/2;
-        while(l+1<r){
+        for(int i=0;i<n;i++)scanf("%d",&cous[i]);
+        long long l=0,r=100000000;
+        long long mid;
+
+        while(l<=r){    
+            mid=(r+l)/2;
             if(check(need,cous,mid)){
-                
+                res=mid;
+                l=mid+1;
             }
+            else r=mid-1;
+            // cout<<l<<' '<<r<<endl;
         }
+        //找sum小于等于 m的x最大值
+        printf("%d\n",res);
     }
-
-
-
 }
--- Xtuoj/1584.cpp
+++ Xtuoj/1584.cpp
@@ -1,25 +1,42 @@
-#include<stdio.h>
-
+#include<iostream>
+#include<queue>
 using namespace std;
 
 // 一个数列a1,a2,…,an，每次你选择执行以下两个操作之一:
 // 取数列中最小的两个数，将两者差的绝对值，再加入数列；
 // 取数列中最大的两个数，将两者差的绝对值，再加入数列；
+// 进行n-1 次操作之后是否只剩下一个元素0
 
-// 0 1 1
+//本题关键 是如何同时取得最大最小,试试双端队列咯!
+
+//2 
+//1 1
 int main(){
 int t;
 scanf("%d",&t);
-//1 2 3 4
-//1 1 2
 while (t--)
 {
+    priority_queue<int> q;
     int n;
     scanf("%d",&n);
-    for(int i=1;i<=n;i++){
-
+    for(int i=0;i<n;i++){
+        int tm;
+        scanf("%d",&tm);
+        q.push(tm);
     }
-
+    int cnt=n-1;
+    while(cnt--){
+        int tm=q.top();
+        q.pop();
+        tm=abs(tm-q.top());
+        q.pop();
+        q.push(tm);
+    }
+    if(q.size()==1){
+    if(q.top()==0){printf("Yes\n");}
+    else printf("No\n");
+    }
+    else printf("No\n");
 }
 
 }
